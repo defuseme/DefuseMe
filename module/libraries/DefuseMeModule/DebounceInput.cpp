dc8744aed5d6ad86a37b2546f9d1129cd3d6eb1c
JK <jkdesign@me.com>
2017-01-05 16:39:56 +0100

    documentation / laser cut files

--- module/libraries/DefuseMeModule/DebounceInput.cpp
+++ module/libraries/DefuseMeModule/DebounceInput.cpp
@@ -1,3 +1,21 @@
+/*
+DebounceInput.cpp - Debounce button and switch input library for project DefuseMe
+Copyright (c) 2016 Nerd2Nerd.org - Jochen Krapf. All right reserved.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 #include "DebounceInput.h"
 
 // defines for state machine - do not change unless you know what you do!
